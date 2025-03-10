1320d843b42f795846609c1bb1c48283babfd4cb
Dang Hoang Duy <e1129930@u.nus.edu>
2025-03-10 23:09:48 +0800

    Add remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,45 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the clinics.
+ */
+public class Remark {
+    public final String remark;
+
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return remark;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return remark.equals(otherRemark.remark);
+    }
+
+    @Override
+    public int hashCode() {
+        return remark.hashCode();
+    }
+}
